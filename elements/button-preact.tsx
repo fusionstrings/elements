4a260f6250dbdce2938f64f2405e665424a0769f
Dilip Kr. Shukla <fusionstrings@gmail.com>
2025-05-02 07:01:03 +0200

    Refactor Button and ButtonPreact components to use class properties for event handling and improve shadow DOM management

--- elements/button-preact.tsx
+++ elements/button-preact.tsx
@@ -3,36 +3,48 @@ import { hydrate, render } from "preact";
 import { Button } from "#button";
 import { count } from "#signals/counter";
 class ButtonPreact extends HTMLElement {
+  static observedAttributes = []; // Add attribute names if needed
+
+  private shadow: ShadowRoot | null = null;
+  private internals?: ElementInternals;
+
   constructor() {
     super();
+    if ("attachInternals" in this) {
+      this.internals = this.attachInternals();
+    }
+  }
 
-    const supportsDeclarative = Object.hasOwn(
-      HTMLElement.prototype,
-      "attachInternals",
-    );
-    const internals = supportsDeclarative ? this.attachInternals() : undefined;
+  private handleClick() {
+    count.value++;
+  }
 
-    // check for a Declarative Shadow Root.
-    let shadow = internals?.shadowRoot;
-
-    if (!shadow) {
-      // there wasn't one. create a new Shadow Root:
-      shadow = this.attachShadow({
-        mode: "open",
-        serializable: true,
-      });
-
+  connectedCallback() {
+    this.shadow = this.internals?.shadowRoot || this.shadowRoot;
+    if (!this.shadow) {
+      this.shadow = this.attachShadow({ mode: "open", serializable: true });
       render(
         <>
           <link rel="stylesheet" href="/templates/button.css" />
-          <Button onClick={() => count.value++} />
+          <Button onClick={this.handleClick} />
         </>,
-        shadow,
+        this.shadow,
       );
     } else {
-      // in either case, wire up our event listener:
-      hydrate(<Button onClick={() => count.value++} />, shadow);
+      hydrate(<Button onClick={this.handleClick} />, this.shadow);
     }
+  }
+
+  disconnectedCallback() {
+    if (this.shadow) {
+      render(null, this.shadow);
+    }
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.log(name)
+    console.log(oldValue)
+    console.log(newValue)
   }
 }
 
--- elements/button.ts
+++ elements/button.ts
@@ -1,43 +1,44 @@
 import { count } from "#signals/counter";
+
 class Button extends HTMLElement {
+  private shadow: ShadowRoot | null = null;
+  private internals?: ElementInternals;
+  private buttonEl: HTMLButtonElement | null = null;
+  private handleClick = () => {
+    count.value++;
+  };
+
   constructor() {
     super();
+    if ("attachInternals" in this) {
+      this.internals = this.attachInternals();
+    }
+  }
 
-    const supportsDeclarative = Object.hasOwn(
-      HTMLElement.prototype,
-      "attachInternals",
-    );
-    const internals = supportsDeclarative ? this.attachInternals() : undefined;
-
-    const onClick = () => {
-      count.value++;
-    };
-
-    // check for a Declarative Shadow Root.
-    let shadow = internals?.shadowRoot;
-
-    if (!shadow) {
-      // there wasn't one. create a new Shadow Root:
-      shadow = this.attachShadow({
-        mode: "open",
-        serializable: true,
-      });
+  connectedCallback() {
+    this.shadow = this.internals?.shadowRoot || this.shadowRoot;
+    if (!this.shadow) {
+      this.shadow = this.attachShadow({ mode: "open", serializable: true });
 
       const template = document.getElementById(
         "template-button",
       ) as HTMLTemplateElement | null;
 
       if (template) {
-        shadow.appendChild(template.content.cloneNode(true));
+        this.shadow.appendChild(template.content.cloneNode(true));
       }
     }
-    // in either case, wire up our event listener:
-    const button = shadow.querySelector("button");
-
-    if (button) {
-      button.addEventListener("click", onClick);
+    // Attach event listener
+    this.buttonEl = this.shadow?.querySelector("button");
+    if (this.buttonEl) {
+      this.buttonEl.addEventListener("click", this.handleClick);
     }
+  }
 
+  disconnectedCallback() {
+    if (this.buttonEl) {
+      this.buttonEl.removeEventListener("click", this.handleClick);
+    }
   }
 }
 
