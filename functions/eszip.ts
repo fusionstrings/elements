59eb196f26972fb20d1f392c9962ba86bd59072f
Dilip Kr. Shukla <fusionstrings@gmail.com>
2025-05-02 07:01:40 +0200

    Refactor eszip.ts to update output file path and remove unused V2 header check

--- functions/eszip.ts
+++ functions/eszip.ts
@@ -7,26 +7,18 @@ import { build, Parser } from 'https://deno.land/x/eszip@v0.84.0/mod.ts';
 const path = new URL('../dom/main.ts', import.meta.url).href;
 console.log(path);
 const importmap = new URL('../deno.json', import.meta.url).href;
-function hasV2Header(bytes: Uint8Array) {
-    const magicV2 = new TextDecoder().decode(bytes.slice(0, 8));
-    // ESZIP_V2_3_MAGIC
-    // https://github.com/denoland/eszip/blob/main/src/v2.rs#L47
-    return magicV2 === "ESZIP2.3";//"ESZIP_V2";
-}
 async function main() {
     const eszip = await build([path], undefined, importmap);
-    await Deno.writeFile('mod.eszip2', eszip);
+    await Deno.writeFile(`package/esm/dom/main.ts.eszip2`, eszip);
 
     const parser = await Parser.createInstance();
     const specifiers = await parser.parseBytes(eszip);
     await parser.load();
-    const specifiers1 = await parser.getModuleSource(specifiers[1]);
-
+    
     const sources = new Map<string, string>();
     for (const specifier of specifiers.sort()) {
         sources.set(specifier, await parser.getModuleSource(specifier));
     }
-    const isV2 = hasV2Header(eszip);
     return eszip;
 }
 
